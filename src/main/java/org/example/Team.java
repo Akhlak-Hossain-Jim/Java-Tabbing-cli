f40e59396951fc8df43b90d2122f7a5355f0cb99
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-09-16 00:55:12 +0600

    🏷️🪲📕🛠️Added and v1 done

---
+++ src/main/java/org/example/Team.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    private String institution;
+    private ArrayList<Debater> Members = new ArrayList<Debater>();
+    private Score score = new Score();
+
+    public Team() {
+    }
+
+    public Team(String n, String in, ArrayList<Debater> a) {
+        this.name = n;
+        this.institution = in;
+        Members = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public Score getScore() {
+        return score;
+    }
+
+    public void display() {
+        System.out.println("\t" + name + score.toString());
+    }
+
+    public String toString() {
+        return "\t" + name + "\t" + institution + "\t" + Members.get(0).getName() + "\t" + Members.get(1).getName()
+                + "\t" + Members.get(2).getName();
+    }
+}
