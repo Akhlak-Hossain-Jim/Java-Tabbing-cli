0869a534286295bd6fec15536a8b8e800f11a6dd
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-09-16 20:56:35 +0600

    🪲fixes

--- src/main/java/org/example/Round.java
+++ src/main/java/org/example/Round.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Round {
     private int roundNo;
@@ -17,89 +19,66 @@ public class Round {
     }
 
     private void Sorter() {
-        for (int i = 0; i < Teams.size() - 1; i++) {
-            for (int j = 0; j < Teams.size() - i - 1; j++) {
-                if ((Teams.get(j).getScore().getTotalScores() > Teams.get(j + 1).getScore().getTotalScores()) &&
-                        (Teams.get(j).getScore().getWins() > Teams.get(j + 1).getScore().getWins())) {
-                    Team cpr = Teams.get(j);
-                    Teams.set(i, Teams.get(i + 1));
-                    Teams.set(i + 1, cpr);
-                }
-            }
-        }
-        for (int i = 0; i < Adj.size() - 1; i++) {
-            for (int j = 0; j < Adj.size() - i - 1; j++) {
-                if (Adj.get(j).getScore().getTotalScores() > Adj.get(j + 1).getScore().getTotalScores()) {
-                    Adjudicator cpr = Adj.get(j);
-                    Adj.set(i, Adj.get(i + 1));
-                    Adj.set(i + 1, cpr);
-                }
-            }
-        }
+    	 Comparator<Team> comparator = Comparator.comparingInt(Team::getTotalScore);
+    	 Collections.sort(Teams, comparator);
+    	 Comparator<Team> comparator2 = Comparator.comparingInt(Team::getWins);
+    	 Collections.sort(Teams, comparator2);
+//    	 for(Team a: Teams) {
+//    		 System.out.println(a.getName()+"\t"+a.getTotalScore()+"\t"+a.getWins());
+//    	 }
+    	 Comparator<Adjudicator> comparatorA = Comparator.comparingInt(Adjudicator::getTotalScore);
+    	 Collections.sort(Adj, comparatorA);
     }
 
     public void Generate() {
+    	System.out.println("\t:........Generating matchup for " + RoundNoDisplay());
         switch (roundNo) {
             case 1: {
                 int q = 0;
-                System.out.println("\t:::::::::Generating matchup for " + RoundNoDisplay());
-                // System.out.println("\tGovernment" + "\t" + "Opposition" + "Place" + "\t" +
-                // "Adjudicators");
                 for (int i = Teams.size() - 1; i >= 0; i -= 2) {
                     MatchUps.add(new MatchUp(Teams.get(i), Teams.get(i - 1), ("0" + (q + 1))));
                     MatchUps.get(q).AddJudge(Adj.get(Adj.size() - 1 - q));
-                    // MatchUps.get(q).display();
                     q++;
                 }
-                System.out.println("\t:::::::::Matchup generation completed for " + RoundNoDisplay());
                 break;
             }
             case 2:
             case 3: {
                 Sorter();
                 int q = 0;
-                System.out.println("\t:::::::::Generating matchup for " + RoundNoDisplay());
-                // System.out.println("\tGovernment" + "\t" + "Opposition" + "Place" + "\t" +
-                // "Adjudicators");
                 for (int i = Teams.size() - 1; i >= 0; i -= 2) {
                     MatchUps.add(new MatchUp(Teams.get(i), Teams.get(i - 1), ("0" + (q + 1))));
                     MatchUps.get(q).AddJudge(Adj.get(Adj.size() - 1 - q));
-                    // MatchUps.get(q).display();
                     q++;
                 }
-                System.out.println("\t:::::::::Matchup generation completed for " + RoundNoDisplay());
                 break;
             }
             case 4: {
                 Sorter();
-                int q = 0;
-                System.out.println("\t:::::::::Generating matchup for " + RoundNoDisplay());
-                // System.out.println("\tGovernment" + "\t" + "Opposition" + "Place" + "\t" +
-                // "Adjudicators");
-                for (int i = Teams.size() - 1; i >= (Teams.size() - 5); i -= 2) {
-                    MatchUps.add(new MatchUp(Teams.get(i), Teams.get(i - 1), ("0" + (q + 1))));
-                    MatchUps.get(q).AddJudge(Adj.get(Adj.size() - 1 - q));
-                    // MatchUps.get(q).display();
-                    q++;
-                }
-                System.out.println("\t:::::::::Matchup generation completed for " + RoundNoDisplay());
+                int s = Teams.size();
+                    MatchUps.add(new MatchUp(Teams.get(s-1), Teams.get(s - 2), ("01")));
+                    MatchUps.get(0).AddJudge(Adj.get(Adj.size() - 1));
+                    MatchUps.get(0).AddJudge(Adj.get(Adj.size() - 2));
+                    MatchUps.get(0).AddJudge(Adj.get(Adj.size() - 3));
+                    
+                    MatchUps.add(new MatchUp(Teams.get(s-3), Teams.get(s - 4), ("02")));
+                    MatchUps.get(1).AddJudge(Adj.get(Adj.size() - 4));
+                    MatchUps.get(1).AddJudge(Adj.get(Adj.size() - 5));
+                    MatchUps.get(1).AddJudge(Adj.get(Adj.size() - 6));
                 break;
             }
             case 5: {
                 Sorter();
-                System.out.println("\t:::::::::Generating matchup for " + RoundNoDisplay());
-                // System.out.println("\tGovernment" + "\t" + "Opposition" + "Place" + "\t" +
-                // "Adjudicators");
                 MatchUps.add(new MatchUp(Teams.get(Teams.size() - 1), Teams.get(Teams.size() - 2), ("01")));
                 MatchUps.get(0).AddJudge(Adj.get(Adj.size() - 1));
                 MatchUps.get(0).AddJudge(Adj.get(Adj.size() - 2));
                 MatchUps.get(0).AddJudge(Adj.get(Adj.size() - 3));
-                System.out.println("\t:::::::::Matchup generation completed for " + RoundNoDisplay());
                 break;
             }
             default:
                 break;
         }
+        System.out.println("\t:::::::::Matchup generation completed for " + RoundNoDisplay());
     }
 
     private String RoundNoDisplay() {
